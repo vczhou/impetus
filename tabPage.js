79fdd4a53f87ef0f9e326c75e473bc80ca525d3e
vczhou <victoria.ch.zhou@gmail.com>
2019-02-09 11:51:32 -0600

    clock updates for real

--- tabPage.js
+++ tabPage.js
@@ -1,7 +1,17 @@
 console.log("HELLO")
+getTime()
 
 let myPopover = document.getElementById('myPopover')
 
-myPopover.onclick = function(element) {
-	element.popover('show')
-};
+function getTime() {
+    console.log("inside time");
+    var time = new Date();
+    var hours = time.getHours();
+    var minutes = time.getMinutes();
+    hours = hours % 12;
+    hours = hours ? hours : 12;
+    minutes = minutes < 10 ? '0' + minutes : minutes;
+    var strTime = hours + ":" + minutes;
+    var t = setTimeout(getTime, 500);
+    document.getElementById('clock').innerText = strTime;
+}
