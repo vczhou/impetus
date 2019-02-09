ca2a3ecb778101be26b155e2bf87c6ec8365d044
Michelle Gu <michelle.gu@utexas.edu>
2019-02-09 15:05:31 -0600

    Added name change capability

--- settings.js
+++ settings.js
@@ -1,5 +1,11 @@
 document.getElementById("back_button").addEventListener("click", goToSettings);
+document.getElementById("update_button").addEventListener("click", changeName);
 
 function goToSettings() {
-		window.open("tabPage.html",'_self') 
+	window.open("tabPage.html",'_self') 
+}
+
+function changeName() {
+	var name = document.getElementById("name_field").value
+	localStorage.setItem("name", name)
 }
